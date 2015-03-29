fb5fd97f68e1b6a019135b943cba3b902b7711b2
Will Coster <willcoster@gmail.com>
2015-03-28 19:03:34 -0700

    Adding deploy script.

---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+Color_Off='\e[0m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+
+if [[ $# -ne 2 ]] ; then
+  echo 'usage: ./deploy sauce-user-name my-sql-password'
+  exit 0
+fi
+
+USER=$1
+PASS=$2
+
+TMP_FILE=`mktemp /tmp/pifuxelck.XXXXXXX.sh`
+cat > $TMP_FILE << EOF
+echo ""
+echo -e "${BYellow}Launching new instance...${Color_Off}"
+cd /srv/pifuxelck/
+/usr/bin/nohup ./pifuxelck-server-go \
+  --port 3002 \
+  --mysql-host db.everythingissauce.com \
+  --mysql-port 3306 \
+  --mysql-user pifuxelck \
+  --mysql-password $PASS \
+  --mysql-db pifuxelck 2>stderr > pifuxelck-server-go.log < /dev/null &
+exit
+EOF
+
+echo ""
+echo -e "${BYellow}Building the executable...${Color_Off}"
+rm pifuxelck-server-go
+go build github.com/GreatestGuys/pifuxelck-server-go
+
+echo ""
+echo -e "${BYellow}Killing existing server instances...${Color_Off}"
+ssh \
+  $USER@everythingissauce.com \
+  "sudo su -c 'killall pifuxelck-server-go' pifuxelck"
+
+echo ""
+echo -e "${BYellow}Deploying the executable...${Color_Off}"
+scp \
+  pifuxelck-server-go \
+  $USER@everythingissauce.com:/srv/pifuxelck/pifuxelck-server-go
+
+echo ""
+echo -e "${BYellow}Deploying start up script...${Color_Off}"
+scp $TMP_FILE $USER@everythingissauce.com:${TMP_FILE}
+
+echo ""
+echo -e "${BYellow}Running start up script...${Color_Off}"
+ssh \
+  $USER@everythingissauce.com \
+  "chmod a+rxw $TMP_FILE && sudo su -c $TMP_FILE pifuxelck && rm $TMP_FILE"
+
+echo ""
+echo -e "${BGreen}All done!${Color_Off}"
+
+rm $TMP_FILE
+exit 0
