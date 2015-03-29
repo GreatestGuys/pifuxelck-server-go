4106e30b7799a59d08c7cf5fed77c23bde6c44a7
Will Coster <willcoster@gmail.com>
2015-03-28 21:58:54 -0700

    Add a metric for uncaught panics.

--- server/handlers/common/metrics.go
+++ server/handlers/common/metrics.go
@@ -3,13 +3,38 @@ package common
 import (
 	"net/http"
 
+	"github.com/GreatestGuys/pifuxelck-server-go/server/log"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 )
+
+var metricUncaughtPanics = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "uncaught_panics",
+		Help: "The number of uncaught panics per endpoint.",
+	},
+	[]string{"handler"},
+)
+
+func init() {
+	prometheus.MustRegister(metricUncaughtPanics)
+}
 
 // InstallHandler installs a handler on a given router. This method will also
 // automatically instrument the handler with prometheus which will automatically
 // report QPS, and quartiles for latency and response size.
 func InstallHandler(r *mux.Router, path string, f http.HandlerFunc) *mux.Route {
-	return r.HandleFunc(path, prometheus.InstrumentHandlerFunc(path, f))
+	metricUncaughtPanics.WithLabelValues(path).Add(0)
+
+	wrapper := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				metricUncaughtPanics.WithLabelValues(path).Inc()
+				log.Errorf("Uncaught panic, %v", r)
+			}
+		}()
+		f(w, r)
+	}
+
+	return r.HandleFunc(path, prometheus.InstrumentHandlerFunc(path, wrapper))
 }
